46ff2f84082162d7ca84d8d2c1d42e27cb4f7c8e
chilikilong112 <chiliki.long@gmail.com>
2022-09-08 13:47:35 +1000

    add front page and questions data

---
+++ script.js
@@ -0,0 +1,41 @@
+//data for the questions
+var data = [
+    {
+        "qs": "Inside which HTML element do we put the JavaScript?",
+        "options": [
+            "<js>",
+            "<javascript>",
+            "<scripting>",
+            "<script>",
+        ],
+        "answerID": 3
+    },
+    {
+        "qs": "Where is the correct place to insert a JavaScript?",
+        "options": [
+            "Both the <head> section and the <body> section are correct",
+            "The <body> section",
+            "The <head> section",
+        ],
+        "answerID": 0
+    },
+    {
+        "qs": "How do you create a function in JavaScript?",
+        "options": [
+            "function:myFunction()",
+            "function myFunction()",
+            "function = myFunction()",
+        ],
+        "answerID": 1
+    },
+    {
+        "qs": "How to write an IF statement in JavaScript?",
+        "option": [
+            "if i = 5 then",
+            "if i = 5",
+            "if (i == 5)",
+            "if i == 5 then",
+        ],
+        "answerID": 2
+    },
+]
